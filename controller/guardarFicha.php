9889b7a6968bd6239de76945ee82eb619b48e8f4
Johan Camilo Guzman Avendaño <jcguzman097@misena.edu.co>
2020-05-26 13:16:00 -0500

    Create guardarFicha.php
    Agrego el controllador de la de la tabla ficha

---
+++ controller/guardarFicha.php
@@ -0,0 +1,37 @@
+<?php  
+	include('Conexion.php');
+	$con = New Conexion();
+ $createcon=$con->conectar();
+
+	if (isset($_POST['ficha'])) {
+
+		$opcion = 'guardar';
+
+		$idPrograma = $_POST['idPrograma'];
+		$fechaInicio = $_POST['fechaInicio'];
+		$fechaFin = $_POST['fechaFin'];
+		$cantidadAprendiz = $_POST['cantidadAprendiz'];
+		$numeroFicha = $_POST['numeroFicha'];
+		
+
+		$sql = "call sp_ficha(null,'$idPrograma','$fechaInicio', '$fechaFin','$cantidadAprendiz','$numeroFicha', '$opcion')";
+
+		$exe = $createcon->query($sql);
+
+		if ($exe ->num_rows > 0) {
+			if ($res = $exe->fetch_row()) {
+				echo $res[0];
+				# code...
+			}else{
+				echo $con->error;
+			}
+			# code...
+		}else{
+			echo "No es posible guardar la ficha";
+		}
+
+	}
+
+?>
+
+
