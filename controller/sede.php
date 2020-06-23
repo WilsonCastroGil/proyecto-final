39b6958d43107022128ba70b8be0d5561bdf4b64
sergio david <sergiose534@gmail.com>
2020-06-23 14:18:17 -0500

    funtion
    se agregan las funciones de actualizar
    sede y centro

--- controller/sede.php
+++ controller/sede.php
@@ -3,6 +3,7 @@
 	$con = New Conexion();
  $createcon=$con->conectar();
 
+function guardarSede(){
  	if (isset($_POST['sede'])) {
 
  		$idMunicipio = $_POST['idMunicipio'];
@@ -31,8 +32,41 @@
 
 	}
 
-?>
+}
 
+function ActualizarSede(){
+	if (isset($_POST['ActualizarSede'])) {
+
+		$idSede =$_POST['idSede'];
+		$idMunicipio = $_POST['idMunicipio'];
+ 		$nombre = $_POST['nombre'];
+ 		$direccion = $_POST['direccion'];
+ 		$telefono = $_POST['telefono'];	
+ 		$correo = $_POST['correo'];
+ 		$director = $_POST['director'];
+ 		$estado = 1;
+
+ 		$sql="UPDATE `sede` SET 
+		`idMunicipio`='$idMunicipio',
+		`nombre`='$nombre',
+		`direccion`='$direccion',
+		`telefono`='$telefono',
+		`correo`='$correo',
+		`director`='$director',
+		`estado`='$estado'
+		WHERE `idSede`='$idSede'";
+
+ 	    $exe = $createcon->query($sql);
+
+		if ($exe) {
+			echo "se guardo";
+			# code...
+		}else{
+			echo "No es posible guardar la sede";
+		}
+	}
+}
+?>
 
 
 
